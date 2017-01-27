b5daade298836d98a122db5e92a96f5faaddef40
Patrick Walsh <patrick@revature.com>
2017-01-27 13:19:05 -0500

    boot mongo docker

--- src/main/java/com/revature/micro/patient/tomcat/web/SampleController.java
+++ src/main/java/com/revature/micro/patient/tomcat/web/SampleController.java
@@ -20,6 +20,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -43,7 +44,7 @@ public class SampleController {
 	}
 	
 	@RequestMapping(value="patient", method=RequestMethod.POST, consumes=MediaType.APPLICATION_JSON_VALUE)
-	public void save(Patient patient){
+	public void save(@RequestBody Patient patient){
 		service.save(patient);
 	}
 	
